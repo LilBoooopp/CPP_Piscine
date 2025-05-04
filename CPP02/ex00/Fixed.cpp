01dbe33841ebee42e1f29d7cf42a70d3a374ddb2
LilBoooopp <cbopp@student.42lausanne.ch>
2025-05-04 16:01:30 +0200

    CPP02 ex00

---
+++ CPP02/ex00/Fixed.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 15:52:41 by cbopp             #+#    #+#             */
+/*   Updated: 2025/05/04 16:00:46 by cbopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _fixedPointValue(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed &other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other) {
+		this->_fixedPointValue = other.getRawBits();
+	}
+	return (*this);
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->_fixedPointValue = raw;
+}
--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -1,33 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbopp <cbopp@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/21 09:37:20 by cbopp             #+#    #+#             */
-/*   Updated: 2025/05/04 15:51:59 by cbopp            ###   ########.fr       */
+/*   Created: 2025/05/04 15:59:41 by cbopp             #+#    #+#             */
+/*   Updated: 2025/05/04 15:59:54 by cbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FIXED_HPP
-# define FIXED_HPP
-
 #include <iostream>
+#include "Fixed.hpp"
 
-class Fixed {
-	private:
-		int					_fixedPointValue;
-		static const int	_fractionalBits = 8;
-
-	public:
-		Fixed(void);
-		Fixed(const Fixed &other);
-		Fixed& operator=(const Fixed &other);
-		~Fixed(void);
-
-		int		getRawBits(void) const;
-		void	setRawBits(int const raw);
-};
-
-#endif
+int	main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
