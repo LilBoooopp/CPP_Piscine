590e70e5148641b5bd1f07bcb229ce9b35c7ac8b
LilBoooopp <cbopp@student.42lausanne.ch>
2025-10-07 21:55:05 +0200

    testing scalar convert

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -1,11 +1,12 @@
 #include "ScalarConverter.hpp"
 #include <iostream>
+#include <cmath>
 
 int	main(int argc, char* argv[]) {
 	if (argc != 2) {
 		std::cerr << "Usage: ./convert <literal>" << std::endl;
 		return (1);
 	}
-	ScalarConverter::convert(argv[1]);
+	ScalarConverter::convert(atof(argv[1]));
 	return (0);
 }
